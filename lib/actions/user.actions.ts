7af06762bf45b63d8019face2d340f2cb8708f29
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2024-03-16 14:54:09 +1100

    fixing path

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,11 +2,11 @@
 
 import { CreateUserParams, UpdateUserParams } from "@/types";
 import { handleError } from "../utils";
-import { connectToDatabase } from "../mongodb/databases";
+import { connectToDatabase } from "../database";
 
-import User from "../mongodb/databases/models/user.model";
-import Event from "../mongodb/databases/models/event.model";
-import Order from "../mongodb/databases/models/order.model";
+import User from "../database/models/user.model";
+import Event from "../database/models/event.model";
+import Order from "../database/models/order.model";
 import { revalidatePath } from "next/cache";
 
 export const createUser = async (user: CreateUserParams) => {
