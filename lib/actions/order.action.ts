8264bf786ab7e12a3f329ecd220128e014f61056
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2024-05-18 12:51:36 +1000

    fix profile miss spell

--- lib/actions/order.action.ts
+++ lib/actions/order.action.ts
@@ -30,7 +30,7 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
         buyerId: order.buyerId,
       },
       mode: "payment",
-      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profie`,
+      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
       cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
     });
     redirect(session.url!);
