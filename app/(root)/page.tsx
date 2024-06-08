d954dfce0d65b6b544d67d1b1eb947e9b5924bc5
NicWirnt <97414933+NicWirnt@users.noreply.github.com>
2024-06-08 17:52:04 +1000

    app done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -61,7 +61,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
           collectionType="All_Events"
-          limit={6}
+          limit={3}
           page={page}
           totalPages={events?.totalPages}
         />
